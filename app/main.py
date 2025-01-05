c0cef1c9e4fef4f68fe8e0679c1d69e1e2303f56
Viktor Stadnik <stadhik.victor@gmail.com>
2025-01-05 20:00:17 +0200

    implemented task "linter-formatter"

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
+def correct_name_dict() -> dict:
+    return {
+        "line_number": "line",
+        "code": "name",
+        "column_number": "column",
+        "text": "message",
+    }
+
+
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        correct_name_dict().get(key, "source"): value
+        if key in correct_name_dict() else "flake8"
+        for key, value in error.items()
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "passed" if not errors else "failed",
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(file, errors)
+        for file, errors in linter_report.items()
+    ]
